687c0f35e0fd3b001bb554bf3c14f9f5afca22fd
nishithbsk <nishith@stanford.edu>
2016-04-19 14:48:44 -0700

    Starting single game DQN pipeline

---
+++ libs/DQN/single_game.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+import os
+import sys
+import argparse
+
+from collections import deque
+from ple import PLE
+from model import *
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='Train a Deep-Q-Network')
+parser.add_argument('-g', '--game', help='Name of game')
+parser.add_argument('-na', '--num_actions', type=int, help='Number of actions')
+args = parser.parse_args()
+
+# Constants
+game_name = args.game # the name of the game being played for log files
+num_actions = args.num_actions # number of valid actions
+batch_size = 32 # size of minibatch
+gamma = 0.99 # decay rate of past observations
+observe = 500 # timesteps to observe before training
+explore = 500 # frames over which to anneal epsilon
+initial_epsilon = 1.0 # starting value of epsilon
+final_epsilon = 0.05 # final value of epsilon
+replay_memory = 590000 # number of previous transitions to remember
+K = 1 # only select an action every Kth frame, repeat prev for others
+
+
